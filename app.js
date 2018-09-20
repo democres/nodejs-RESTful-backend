7fe3fde517188ac35965cbbd5fa7a03dc63b69df
root <root@localhost.localdomain>
2018-09-19 20:20:49 -0400

    Code refactor Login Security improvement

--- app.js
+++ app.js
@@ -1,58 +1,26 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 
-//connect to MongoDB
-mongoose.connect('mongodb://david:David123@ds163382.mlab.com:63382/davidtestdb');
-var db = mongoose.connection;
-
-//handle mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
-});
-
-//use sessions for tracking logins
-app.use(session({
-  secret: 'work hard',
-  resave: true,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: db
-  })
-}));
-
-// parse incoming requests
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
+// use JWT auth to secure the api
+app.use(jwt());
 
-// serve static files from template
-app.use(express.static(__dirname + '/templateLogReg'));
+// api routes
+app.use('/users', require('./users/users.controller'));
 
-// include routes
-var routes = require('./routes/router');
-app.use('/', routes);
+// global error handler
+app.use(errorHandler);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('File Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-// define as the last app.use callback
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.send(err.message);
-});
-
-
-// listen on port 3000
-app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
