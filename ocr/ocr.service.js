44f20d5c5b35baeaf353338d44574203fbf469a7
David Figueroa <democres@hotmail.es>
2019-04-26 11:53:12 -0500

    parse data

--- ocr/ocr.service.js
+++ ocr/ocr.service.js
@@ -29,7 +29,7 @@ https.get('https://jsonplaceholder.typicode.com/todos/1', (resp) => {
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
     console.log(JSON.parse(data));
-    return(data)
+    return(JSON.parse(data))
   });
 
 }).on("error", (err) => {
