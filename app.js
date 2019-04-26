ed02212916c39d889ea02945b1dae3b60c69b8be
David Figueroa <democres@hotmail.es>
2019-04-26 11:48:10 -0500

    Change port

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/ocr', require('./ocr/ocr.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const port = process.env.NODE_ENV === 'production' ? 80 : 3333;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
